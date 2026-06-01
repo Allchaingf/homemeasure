27bec2cf79450ed248ad933d0d61e0a593174e92
anton0801 <danilovanton724@gmail.com>
2026-06-01 20:05:35 +0200

    Initial Commit

---
+++ HomeMeasure/HomeMeasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeMeasureApp.swift
+//  HomeMeasure
+//
+//  Created by Антон on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct HomeMeasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
